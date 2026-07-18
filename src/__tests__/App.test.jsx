abda46156d918bf83d260c110ade43c102eb34c8
jhjulialee <jl4434@nyu.edu>
2026-07-18 13:51:41 -0400

    Lab: Optimism and Reusability - Form Storage

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,1 +1,50 @@
-import './test_suites/CustomHooks.test'
+import { fireEvent, render, renderHook } from "@testing-library/react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import App from "../components/App";
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
+
+beforeEach(() => {
+  localStorage.clear();
+});
+
+describe("Custom Hooks", () => {
+  test("returns an initial state and a setter function", () => {
+    const { result } = renderHook(() => useLocalStorage("test", "value"));
+    expect(result.current).toMatchObject(["value", expect.any(Function)]);
+  });
+
+  test("has an initial value of null of no value is passed in as a second argument", () => {
+    const { result } = renderHook(() => useLocalStorage("test"));
+    expect(result.current).toMatchObject([null, expect.any(Function)]);
+  });
+
+  test("saves the value in localStorage when state is updated", async () => {
+    const { findAllByTestId } = render(<App />);
+    const input = await findAllByTestId("name");
+    fireEvent.change(input[0], { target: { value: "New Value" } });
+    expect(localStorage.getItem("name")).toBe("New Value");
+  });
+});
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,14 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+  const [storedValue, setStoredValue] = useState(() => {
+    const savedValue = localStorage.getItem(key);
+    return savedValue !== null ? savedValue : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, storedValue);
+  }, [key, storedValue]);
+
+  return [storedValue, setStoredValue];
 }
